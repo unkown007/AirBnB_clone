2191b247efe757ba344e07b9f7c52feef63eb9d4
unkown007 <victormatusse00@gmail.com>
2023-05-14 21:52:05 +0000

    added file

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+""" unittest """
+
+
+import unittest
+from models.base_model import BaseModel
+from models import storage
+from models.engine.file_storage import FileStorage
+import os
+
+
+class TestBase(unittest.TestCase):
+    """ test cases """
+
+    @classmethod
+    def setUpClass(cls):
+        """ setup BaseModel instance"""
+        cls.ins = BaseModel()
+
+    @classmethod
+    def teardown(cls):
+        """ cleaning up all test working tools """
+        del cls.ins
+        try:
+            os.remove("file.json")
+        except Exception:
+            pass
+
+    def test_BaseModelAttr(self):
+        """ Testing BaseModel attributes """
+        self.assertEqual(hasattr(self.ins, 'id'), True)
+        self.assertEqual(hasattr(self.ins, 'created_at'), True)
+        self.assertEqual(hasattr(self.ins, 'updated_at'), True)
+
+    def test_instance(self):
+        """ Testing BaseModel instance """
+        self.assertTrue(isinstance(self.ins, BaseModel))
+
+
+    def test_save_updated_at_created_at(self):
+        """ Testing the save updated_at and created_at attributes """
+        self.ins.save()
+        self.assertNotEqual(self.ins.created_at, self.ins.updated_at)
+        dummy = BaseModel()
+        id_t = dummy.id
+        dummy.name = "Victor"
+        dummy.save()
+        storage.reload()
+        obj = storage.all()[f"BaseModel.{id_t}"]
+        self.assertTrue(hasattr(obj, "name"))
+        self.assertTrue(obj.name == "Victor")
+        self.assertTrue(os.path.exists("file.json"))
+
+    def test_dic(self):
+        """ Testing dictionary representation """
+        dic = self.ins.to_dict()
+        self.assertTrue(dic.get("__class__"))
+        self.assertTrue(type(dic) is dict)
+        self.assertTrue("to_dict" in dir(self.ins))
+
+    def test_storage(self):
+        """ Testing storage """
+        objs = storage.all()
+
+        self.assertTrue(type(objs) is dict)
+        self.assertTrue(isinstance(storage, FileStorage))
+
+    def test_reaload(self):
+        """ Testing reloag of objs in file.json """
+        self.ins.save()
+        storage.reload()
+        dic = storage.all()
+        self.assertTrue(len(dic) != 0)
+
+if __name__ == "__main__":
+    unittest.main()
